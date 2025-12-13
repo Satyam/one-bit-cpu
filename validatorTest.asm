07ad8b8e0c6a825ab4e2e348df36482a4e5bad9e
Satyam <satyam@satyam.com.ar>
2025-12-13 19:11:59 +0100

    Started on the assembler, tested validation.

---
+++ validatorTest.asm
@@ -0,0 +1,19 @@
+# some comment
+   #some more comment
+   and 
+   AND
+   ANT
+   And 25
+label and 25
+1label and 25
+label and 25  #this is repeated
+label and 25#this is repeated
+label and 25  this is repeated
+ not
+ not 12
+ or 0b1000
+ or 0o456
+ or 0xaf89
+ or lbl
+ or 0o999
+ or 0b2
