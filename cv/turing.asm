7518413821f9a978369759eb5aa08c299ae65e27
Satyam <satyam@satyam.com.ar>
2026-04-28 18:06:38 +0200

    tidied up `turing.asm` and added the `Busy Beaver` program

--- cv/turing.asm
+++ cv/turing.asm
@@ -1,11 +1,11 @@
 # https://en.wikipedia.org/wiki/Turing_machine_examples#Turing's_first_example
 
 # Configuration    Behavior
-# state    Tape symbol    Tape operations    new state
-#   b          blank        P0, R             c
-#   c          blank        R                 e
-#   e          blank        P1, R             f
-#   f          blank        R                 b
+# state        symbol    Print  move    new state
+#   b          blank        0   R             c
+#   c          blank            R             e
+#   e          blank        1   R             f
+#   f          blank            R             b
 
 # The issue is that blank, 0 and 1 are three states so it is not binary
 # I changed so that 0 is blank, which means it will have 1's spaced three 0's apart
@@ -21,33 +21,37 @@ b:    jmpt  end
       zero
       inNext
       jmp   c
+
 c:    jmpt  end
       inNext
       jmp   e
+
 e:    jmpt  end
       one
       out
       inNext
       jmp   f
+
 f:    jmpt  end
       inNext
       jmp   b
-end: halt
+
+end:  halt
 
 # https://en.wikipedia.org/wiki/Turing_machine_examples#A_copy_subroutine
 
 
 #  State  Symbol   Print Operation Next State
-#   s1      0        N       N            H
-#   s1      1        E       R            s2
-#   s2      0        E       R            s3
-#   s2      1        P1      R            s2
-#   s3      0        P1      L            s4
-#   s3      1        P1      R            s3
-#   s4      0        E       L            s5
-#   s4      1        P1      L            s4
-#   s5      0        P1      R            s1
-#   s5      1        P1      L            s5
+#   s1      0        -       -            H
+#   s1      1        0       R            s2
+#   s2      0        0       R            s3
+#   s2      1        1       R            s2
+#   s3      0        1       L            s4
+#   s3      1        1       R            s3
+#   s4      0        0       L            s5
+#   s4      1        1       L            s4
+#   s5      0        1       R            s1
+#   s5      1        1       L            s5
 #   H       —        —       —
 
 # Setup:
@@ -60,59 +64,124 @@ end: halt
 
 s1:   jmpt s1_1
 
-#   s1    0      N       N            H
+#   s1    0      -       -            H
 s1_0: halt
 
-#   s1    1      E       R            s2
-s1_1:  zero
+#   s1    1      0       R            s2
+s1_1: zero
       out
-      inNext
+      inPrev
       jmp s2
 
 s2:   jmpt s2_1
-#   s2    0      E       R            s3
-s2_0:  zero
+
+#   s2    0      0       R            s3
+s2_0: zero
       out
-      inNext
+      inPrev
       jmp s3
-#   s2    1      P1       R            s2
+
+#   s2    1      1       R            s2
 s2_1: one
       out
-      inNext
+      inPrev
       jmp s2
 
 s3:   jmpt  s3_1
-#   s3    0      P1       L            s4
+
+#   s3    0      1       L            s4
 s3_0: one
       out
-      inPrev
+      inNext
       jmp s4
-#   s3    1      P1       R            s3
+
+#   s3    1      1       R            s3
 s3_1: one
       out
-      inNext
+      inPrev
       jmp s3
 
 s4:   jmpt  s4_1
-#   s4    0      E       L            s5
+#   s4    0      0       L            s5
 s4_0: zero
       out
-      inPrev
+      inNext
       jmp s5
-#   s4    1      P1       L            s4
+
+#   s4    1      1       L            s4
 s4_1: one
       out
-      inPrev
+      inNext
       jmp   s4
 
 s5:   jmpt  s5_1
-#   s5    0      P1       R            s1
+
+#   s5    0      1       R            s1
 s5_0: one
       out
-      inNext
+      inPrev
       jmp   s1
-#   s5    1      P1       L            s5
+
+#   s5    1      1       L            s5
 s5_1: one
       out
-      inPrev
+      inNext
       jmp   s5
+
+# https://en.wikipedia.org/wiki/Turing_machine_examples#3-state_Busy_Beaver
+# Busy beaver
+
+#  State  Symbol   Print Operation Next State
+#   A        0       1       L         B
+#   A        1       1       R         C
+#   B        0       1       R         A
+#   B        1       1       L         B
+#   C        0       1       R         B
+#   C        1       1       -         Halt
+
+# setup
+      org 128
+      inFrom  8
+
+# run
+
+A:    jmpt  A_1
+
+#   A        0       1       L         B
+A_0:  one
+      out
+      inNext
+      jmp   B
+
+#   A        1       1       R         C
+A_1:  one
+      out
+      inPrev
+      jmp C
+
+B:    jmpt  B_1
+
+#   B        0       1       R         A
+B_0:  one
+      out
+      inPrev
+      jmp A
+
+#   B        1       1       L         B
+B_1:  one
+      out
+      inNext
+      jmp   B
+
+C:    jmpt  C_1
+
+#   C        0       1       R         B
+C_0:  one
+      out
+      inPrev
+      jmp B
+
+#   C        1       1       -         Halt
+C_1:  one
+      out
+      halt
